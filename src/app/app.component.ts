ea151946844a801bb5d5647a79c555765c9e2971
MaxShevchenko23 <maksimsevcenko250@gmail.com>
2024-09-03 21:50:08 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+import { MainPageComponent } from './main-page/main-page.component';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MainPageComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'url-shortener-front';
+  title = 'url-shortener-front'; 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { MainPageComponent } from "./main-page/main-page.component";
+import { Routes } from "@angular/router";
+import { AuthComponent } from "./auth/auth.component";
+import { AboutComponent } from "./about/about.component";
+import { LinkTableComponent } from "./link-table/link-table.component";
+import { LinkInfoComponent } from "./link-info/link-info.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'home', component: MainPageComponent },
+    { path: 'login', component: AuthComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'links', component: LinkTableComponent },
+    { path: 'info', component: LinkInfoComponent},
+    { path: 'info/:link', component: LinkInfoComponent},
+  ];
